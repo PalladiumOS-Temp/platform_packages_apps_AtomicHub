8f4873826f05968710f17bc45285f8ad2437b987
bigrushdog <randall.rushing@gmail.com>
2020-12-09 14:43:07 -0500

    Atomichub: Add navigation bar visibility control [2/2]
    Change-Id: I5eb9f3e452102c62555fb117256abf464ee5c2b5

--- src/com/palladium/atomichub/categories/frag_misc.java
+++ src/com/palladium/atomichub/categories/frag_misc.java
@@ -2,6 +2,7 @@ package com.palladium.atomichub.categories;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.Handler;
 import android.content.Context;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
@@ -16,15 +17,22 @@ import com.android.settings.Utils;
 import android.os.ServiceManager;
 import com.palladium.atomichub.*;
 import android.app.ActionBar;
+import android.provider.Settings;
 import com.android.internal.util.custom.FodUtils;
+import com.android.internal.util.palladium.PalladiumUtils;
 
 public class frag_misc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+    private static final String KEY_FOD_RECOGNIZING_ANIM = "fod_recognizing_animation";
 
     private IOverlayManager mOverlayService;
     private boolean mHasFod;
     private Context mContext;
+    private SwitchPreference mNavbarVisibility;
 
-    private static final String KEY_FOD_RECOGNIZING_ANIM = "fod_recognizing_animation";
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,11 +43,22 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
         //Feature Additon!
         mContext = getContext();
         mHasFod = FodUtils.hasFodSupport(mContext);
+        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         if (!mHasFod) {
             prefScreen.removePreference(findPreference(KEY_FOD_RECOGNIZING_ANIM));
         }
+
+        boolean defaultToNavigationBar = PalladiumUtils.deviceSupportNavigationBar(getActivity());
+        boolean showing = Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0) != 0;
+        updateBarVisibleAndUpdatePrefs(showing);
+
+        mNavbarVisibility.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
 
     }
     @Override
@@ -61,6 +80,28 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String key = preference.getKey();
+        if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
+                    showing ? 1 : 0);
+            updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
         return true;
     }
+
+    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
+        mNavbarVisibility.setChecked(showing);
+    }
+
 }
