6f5351f8b8e67883d3e28457431f2a326047fb0e
bigrushdog <randall.rushing@gmail.com>
2020-12-09 14:43:07 -0500

    Atomichub: Add navigation bar visibility control [2/2]
    Change-Id: I5eb9f3e452102c62555fb117256abf464ee5c2b5

--- src/com/palladium/atomichub/categories/frag_misc.java
+++ src/com/palladium/atomichub/categories/frag_misc.java
@@ -2,6 +2,7 @@ package com.palladium.atomichub.categories;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.Handler;
 import android.content.Context;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
@@ -16,12 +17,19 @@ import android.os.ServiceManager;
 import com.palladium.atomichub.*;
 import android.app.ActionBar;
 import com.palladium.atomichub.preference.SystemSettingMasterSwitchPreference;
+import android.provider.Settings;
+import com.android.internal.util.palladium.PalladiumUtils;
 
 public class frag_misc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+
     private IOverlayManager mOverlayService;
     private Context mContext;
+    private SwitchPreference mNavbarVisibility;
 
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,19 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         //Feature Additon!
+        mContext = getContext();
+        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        boolean defaultToNavigationBar = PalladiumUtils.deviceSupportNavigationBar(getActivity());
+        boolean showing = Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0) != 0;
+        updateBarVisibleAndUpdatePrefs(showing);
+
+        mNavbarVisibility.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
 
     }
     @Override
@@ -51,6 +72,28 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String key = preference.getKey();
+        if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
+                    showing ? 1 : 0);
+            updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
         return true;
     }
+
+    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
+        mNavbarVisibility.setChecked(showing);
+    }
+
 }
