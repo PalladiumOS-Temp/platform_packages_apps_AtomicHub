5f1a7637e5ad69532b26d22baec25ea10cd8f3cf
AgentFabulous <kshitijgm@gmail.com>
2020-12-30 19:47:53 +0530

    Atomichub: RGB Accent Picker [2/2]

--- src/com/palladium/atomichub/categories/frag_theme.java
+++ src/com/palladium/atomichub/categories/frag_theme.java
@@ -1,11 +1,18 @@
 package com.palladium.atomichub.categories;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.graphics.Color;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
 import com.android.settings.R;
@@ -14,16 +21,24 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 import android.os.ServiceManager;
 import android.app.ActionBar;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import com.palladium.atomichub.*;
+import androidx.preference.*;
 import androidx.fragment.app.FragmentManager;
 import com.android.settings.display.OverlayCategoryPreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class frag_theme extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    private static final String ACCENT_COLOR = "accent_color";
+    private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
     private IOverlayManager mOverlayService;
-
+    private ColorPickerPreference mThemeColor;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +46,8 @@ public class frag_theme extends SettingsPreferenceFragment implements OnPreferen
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         //Feature Additon!
+         setupAccentPref();
 
-    }
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.PALLADIUM;
     }
 
     @Override
@@ -62,11 +74,35 @@ public class frag_theme extends SettingsPreferenceFragment implements OnPreferen
     public void onPause() {
         super.onPause();
     }
-
+    
+    private void setupAccentPref() {
+        mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
+        String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? Color.WHITE
+                : Color.parseColor("#" + colorVal);
+        mThemeColor.setNewPreviewColor(color);
+        mThemeColor.setOnPreferenceChangeListener(this);
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String key = preference.getKey();
+        if (preference == mThemeColor) {
+            int color = (Integer) objValue;
+            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            SystemProperties.set(ACCENT_COLOR_PROP, hexColor);
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
+        }
         return true;
+    }
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.PALLADIUM;
     }
 }
