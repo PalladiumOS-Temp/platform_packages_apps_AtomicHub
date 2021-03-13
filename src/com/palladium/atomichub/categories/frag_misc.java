347c2583cdfdcec00dcdea45159d9516ac556d45
pastime1971 <pastime1971@gmail.com>
2021-03-13 09:59:19 +0530

    Atomichub: Add option to disable scrolling cache [2/2]

--- src/com/palladium/atomichub/categories/frag_misc.java
+++ src/com/palladium/atomichub/categories/frag_misc.java
@@ -13,13 +13,22 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 import android.os.ServiceManager;
+import androidx.preference.*;
 import com.palladium.atomichub.*;
 import android.app.ActionBar;
 
+import com.palladium.atomichub.preference.SecureSettingListPreference;
+import android.os.SystemProperties;
 public class frag_misc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private IOverlayManager mOverlayService;
     private Context mContext;
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "2";
+
+    private ListPreference mScrollingCachePref;
+
 
     private static final String KEY_FOD_RECOGNIZING_ANIM = "fod_recognizing_animation";
 
@@ -31,6 +40,11 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         //Feature Additon!
         mContext = getContext();
+	    mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+        SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
+
     }
     @Override
     public int getMetricsCategory() {
@@ -51,6 +65,12 @@ public class frag_misc extends SettingsPreferenceFragment implements OnPreferenc
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String key = preference.getKey();
+        if (preference == mScrollingCachePref) {
+            if (newValue != null) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String) newValue);
+            }
+            return true;
+	}
         return true;
     }
 }
